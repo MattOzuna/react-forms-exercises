00af5d544d66d68706be9de7113343af896426de
MattOz <m.ozuna22@gmail.com>
2024-04-07 12:47:03 -0400

    added functionality test to Todolist test

--- src/Todo-components/NewTodoForm.test.js
+++ src/Todo-components/NewTodoForm.test.js
@@ -1,12 +1,12 @@
-import NewTodoForm from "./NewTodoForm.js"
-import '@testing-library/jest-dom';
+import NewTodoForm from "./NewTodoForm.js";
+import "@testing-library/jest-dom";
 import { render, asFragment } from "@testing-library/react";
 
 it("should render", () => {
-    render(<NewTodoForm />)
-})
+  render(<NewTodoForm />);
+});
 
-it('should match Snapshot', () =>{
-    const {asFragment} = render(<NewTodoForm/>)
-    expect(asFragment()).toMatchSnapshot()
-})
+it("should match Snapshot", () => {
+  const { asFragment } = render(<NewTodoForm />);
+  expect(asFragment()).toMatchSnapshot();
+});
--- src/Todo-components/TodoList.test.js
+++ src/Todo-components/TodoList.test.js
@@ -1,13 +1,30 @@
-import { render, asFragment } from "@testing-library/react"
-import TodoList from "./TodoList"
-import '@testing-library/jest-dom';
+import { render, asFragment, fireEvent } from "@testing-library/react";
+import TodoList from "./TodoList";
+import "@testing-library/jest-dom";
 
+it("should render", () => {
+  render(<TodoList />);
+});
 
-it("should render", ()=>{
-    render(<TodoList />);
-})
+it("should match snapshot", () => {
+  const { asFragment } = render(<TodoList />);
+  expect(asFragment()).toMatchSnapshot();
+});
 
-it('should match snapshot', () =>{
-    const {asFragment} = render(<TodoList />);
-    expect(asFragment()).toMatchSnapshot();
+it("should add a new Todo item to list", () => {
+  const { getByLabelText, queryByText } = render(<TodoList />);
+
+  expect(queryByText("Test Task")).not.toBeInTheDocument();
+
+  const taskInput = getByLabelText("New Task:");
+  const addButton = queryByText("Add");
+
+  fireEvent.change(taskInput, { target: { value: "Test Task" } });
+  fireEvent.click(addButton);
+
+  expect(queryByText("Test Task")).toBeInTheDocument();
+});
+
+it('should remove a todo item with the "X" button', () => {
+    const {getByLabelText, queryByText} = render(<TodoList />)
 })
